9250673b760bc1e9550c714e9f9424661c8bf4da
sasaplus1 <sasaplus1@gmail.com>
2025-10-04 00:35:10 +0900

    add init.sh

---
+++ init.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+set -euo pipefail
+
+__main() {
+  unset -f __main
+
+  local os=
+  case "$(uname -s)" in
+    Darwin)
+      os=macos
+      ;;
+    Linux)
+      os=linux
+      ;;
+    *)
+      echo "Unsupported OS: $(uname -s)" >&2
+      exit 1
+      ;;
+  esac
+
+  local arch=
+  case "$(uname -m)" in
+    aarch64|arm64)
+      arch=aarch64
+      ;;
+    x86_64|amd64)
+      arch=x86_64
+      ;;
+    *)
+      echo "Unsupported architecture: $(uname -m)" >&2
+      exit 1
+      ;;
+  esac
+
+  gh release download --repo extism/js-pdk v1.5.1 --pattern "*-${arch}-${os}-*" --skip-existing
+
+  local -r file="$(basename "$(find . -maxdepth 1 -name "*-${arch}-${os}-*.gz")")"
+  printf -- '  %s' "$file" >> "${file}.sha256"
+
+  local shasum=
+  type shasum &>/dev/null && shasum='shasum -a 256'
+  type sha256sum &>/dev/null && shasum=sha256sum
+  if [ -z "$shasum" ]
+  then
+    echo "Neither shasum nor sha256sum is available" >&2
+    exit 1
+  fi
+
+  if "$shasum" --check --status "${file}.sha256"
+  then
+    echo "Checksum verification succeeded"
+  else
+    echo "Checksum verification failed" >&2
+    exit 1
+  fi
+
+  gunzip "$file"
+
+  local -r base="${file%.gz}"
+  chmod +x "$base"
+
+  [ "$os" == 'macos' ] && xattr -c "$base"
+
+  mv "$base" extism-js
+}
+__main "$@"
